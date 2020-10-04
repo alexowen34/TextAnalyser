1a2c69cc3f4f12a22486ae743b92ef2c4f0a172c
alexowen34 <72303298+alexowen34@users.noreply.github.com>
2020-10-04 20:37:45 +0100

    Changed public methods to private where appropriate following FXML to pure Java code migration.

--- TextAnalyserTool/src/main/java/codingAssignment/GUI.java
+++ TextAnalyserTool/src/main/java/codingAssignment/GUI.java
@@ -1,6 +1,6 @@
 //TO DO:
 
-//test
+//testtest
 //#1: Sort out issue with importing text from text file - can't sort, add decimals etc.
 //#2: Organise back end to make it more efficent
 //#3  Comment code
