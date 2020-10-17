8f58023d1ebfedb75a1f3cfc395c6c5fa00fdbef
alexowen34 <72303298+alexowen34@users.noreply.github.com>
2020-10-17 19:37:46 +0100

    Forgot to stage a previous change.

---
+++ TextAnalyserTool/src/main/java/codingAssignment/DecimalPlaces.java
@@ -0,0 +1,18 @@
+package codingAssignment;
+
+public class DecimalPlaces 
+{	
+	public String stringRepresentationOfDecimals()
+	{
+		String decimalPlacesString = "0";
+		if(GUI.numberOfDecimalPlaces > 0)
+		{
+			decimalPlacesString = "0.";
+			for(int i = 1; i <= GUI.numberOfDecimalPlaces; i++)
+			{
+				decimalPlacesString += "0";
+			}
+		}
+		return decimalPlacesString;
+	}
+}
